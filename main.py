06834dd9b9e996e2f887f60f1e16318ca474dff0
brianxiang123 <50874585+brianxiang123@users.noreply.github.com>
2022-02-27 19:31:56 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import numpy as np
+import math
+
+def main():
+    dminh = np.load('./Tasks/Direction_task_with_dots_synchronised_min_hilbert.npz')
+    dmin = np.load('./Tasks/Direction_task_with_dots_synchronised_min.npz')
+    dmaxh = np.load('./Tasks/Direction_task_with_dots_synchronised_max_hilbert.npz')
+    dmax = np.load('./Tasks/Direction_task_with_dots_synchronised_max.npz')
+    pminh = np.load('./Tasks/Position_task_with_dots_synchronised_max.npz')
+    pmin = np.load('./Tasks/Position_task_with_dots_synchronised_max.npz')
+    pmaxh = np.load('./Tasks/Position_task_with_dots_synchronised_max.npz')
+    pmax = np.load('./Tasks/Position_task_with_dots_synchronised_max.npz')
+
+    ldminh = dminh['labels']
+    ldmin = dmin['labels']
+    ldmaxh = dmaxh['labels']
+    ldmax = dmax['labels']
+    lpminh = pminh['labels']
+    lpmin = pmin['labels']
+    lpmaxh = pmaxh['labels']
+    lpmax = pmax['labels']
+
+    print("cdminh")
+    cdminh = leftrighttrans1(ldminh[:,2])
+    print("cdmin")
+    cdmin = leftrighttrans1(ldmin[:,2])
+    print("cdmaxh")
+    cdmaxh = leftrighttrans1(ldmaxh[:,2])
+    print("cdmax")
+    cdmax = leftrighttrans1(ldmax[:,2])
+
+    ndminh = np.vstack((ldminh[:,0], cdminh)).T
+    ndmin = np.vstack((ldmin[:,0], cdmin)).T
+    ndmaxh = np.vstack((ldmaxh[:,0], cdmaxh)).T
+    ndmax = np.vstack((ldmax[:,0], cdmax)).T
+
+    np.savez('./Tasks/LR_task_with_dots_synchronised_min_hilbert.npz', EEG = dminh['EEG'], labels = ndminh)
+    np.savez('./Tasks/LR_task_with_dots_synchronised_min.npz', EEG = dmin['EEG'], labels = ndmin)
+    np.savez('./Tasks/LR_task_with_dots_synchronised_max_hilbert.npz', EEG = dmaxh['EEG'], labels = ndmaxh)
+    np.savez('./Tasks/LR_task_with_dots_synchronised_max.npz', EEG = dmax['EEG'], labels = ndmax)
+
+
+def leftrighttrans1(rarr):
+    narr = []
+    cnt1 = 0
+    cnt2 = 0
+    for i in range(len(rarr)):
+        if abs(rarr[i]) < math.pi/2:
+            narr.append(1)
+            cnt1+=1
+        else:
+            narr.append(0)
+            cnt2+=1
+    print("Right: " + str(cnt1))
+    print("Left: " + str(cnt2))
+    return narr
+
+main()
