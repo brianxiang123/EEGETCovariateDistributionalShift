0361358293d5ef00ec4744e60f1477386ec1c60f
brianxiang123 <50874585+brianxiang123@users.noreply.github.com>
2022-05-26 22:54:17 -0400

    Update TranslatingLR.py

--- TranslatingLR.py
+++ TranslatingLR.py
@@ -2,35 +2,15 @@ import numpy as np
 import math
 
 def main():
-    dminh = np.load('./Tasks/Direction_task_with_dots_synchronised_min_hilbert.npz')
     dmin = np.load('./Tasks/Direction_task_with_dots_synchronised_min.npz')
-    dmaxh = np.load('./Tasks/Direction_task_with_dots_synchronised_max_hilbert.npz')
-    dmax = np.load('./Tasks/Direction_task_with_dots_synchronised_max.npz')
 
-    ldminh = dminh['labels']
     ldmin = dmin['labels']
-    ldmaxh = dmaxh['labels']
-    ldmax = dmax['labels']
 
-    print("cdminh")
-    cdminh = leftrighttrans1(ldminh[:,2])
-    print("cdmin")
     cdmin = leftrighttrans1(ldmin[:,2])
-    print("cdmaxh")
-    cdmaxh = leftrighttrans1(ldmaxh[:,2])
-    print("cdmax")
-    cdmax = leftrighttrans1(ldmax[:,2])
 
-    ndminh = np.vstack((ldminh[:,0], cdminh)).T
     ndmin = np.vstack((ldmin[:,0], cdmin)).T
-    ndmaxh = np.vstack((ldmaxh[:,0], cdmaxh)).T
-    ndmax = np.vstack((ldmax[:,0], cdmax)).T
 
-    np.savez('./Tasks/LR_task_with_dots_synchronised_min_hilbert.npz', EEG = dminh['EEG'], labels = ndminh)
     np.savez('./Tasks/LR_task_with_dots_synchronised_min.npz', EEG = dmin['EEG'], labels = ndmin)
-    np.savez('./Tasks/LR_task_with_dots_synchronised_max_hilbert.npz', EEG = dmaxh['EEG'], labels = ndmaxh)
-    np.savez('./Tasks/LR_task_with_dots_synchronised_max.npz', EEG = dmax['EEG'], labels = ndmax)
-
 
 def leftrighttrans1(rarr):
     narr = []
